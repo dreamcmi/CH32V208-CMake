a7cc2c75999131b20848fb104183e10b30c3d893
dreamcmi <1912544842@qq.com>
2022-10-13 03:06:42 +0800

    fix cmd

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 if [ -d "build/" ]; then
     echo "Found build/"
     cd build
-    cmake -GNinja ..
+    cmake -GNinja -DBUILD_WITH_BLE=ON ..
     ninja
 else
     echo "Not Found build/"
     mkdir build
     cd build
-    cmake -GNinja ..
+    cmake -GNinja -DBUILD_WITH_BLE=ON ..
     ninja
 fi
